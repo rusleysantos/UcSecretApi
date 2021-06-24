579f6213fbe99b51b43b974afe0250d17e8eef48
Rusley Santos <rusley.santos@uniodigital.com.br>
2021-06-23 22:06:34 -0300

    criar usuario

--- PerguntaSocoApi/Controllers/UsuarioController.cs
+++ PerguntaSocoApi/Controllers/UsuarioController.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Domains;
 using Microsoft.AspNetCore.Mvc;
+using Repository.Contracts;
+using Repository.Models;
 
 namespace UcSecretApi.Controllers
 {
+    [Route("api")]
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        private IUsuarioService _service { get; }
+        public UsuarioController(IUsuarioService service)
         {
-            return View();
+            _service = service;
+        }
+
+        public async Task<IActionResult> CriarUsuario([FromBody] Usuario usuario)
+        {
+            try
+            {
+                if (await _service.CriarUsuario(usuario))
+                {
+                    return Ok(new MessageReturn("Sucesso",
+                                                "",
+                                                true));
+                }
+                else
+                {
+                    return Ok(new MessageReturn("Não foi possível",
+                                                "erro",
+                                                false));
+
+                }
+            }
+            catch
+            {
+                return BadRequest(new MessageReturn("Erro",
+                                                     "Erro",
+                                                     false));
+
+            }
         }
     }
 }
